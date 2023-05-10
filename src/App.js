7cc3edcc36fbf7e343f961fe4cd702566e763189
Jesus <117941643+JesusV545@users.noreply.github.com>
2023-05-09 22:56:42 -0500

    fixes for app.js and index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+// import Project from './components/Project';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=' bg-cyan-900 m-0 min-h-screen flex flex-col'> 
+      <Header title = 'Jesus A. Vazquez' className='text-gray-200'/>
+
+      <Navigation />
+
+      <Footer />
     </div>
+
   );
+
 }
 
 export default App;
